33ed48874492d62f1b9c1eef9558a4bf30070fa3
Pascal Rainville <pascal.rainville@gmail.com>
2019-09-07 22:36:08 -0400

    ...

--- private/app.js
+++ private/app.js
@@ -4,7 +4,15 @@
 
 $.getJSON('http://localhost:3000/files', function(data) {
   const items = data.map(function(elem) {
-    const val = '<li><a href="../data/'+encodeURIComponent(elem)+'">🔎</a> <a href="../data/'+encodeURIComponent(elem)+'?contentType=text">📖</a> <a href="edit/'+encodeURIComponent(elem)+'">✏️</a> &nbsp; &#x1f4c4;<span class="size">174 bytes</span><input type="text" name="filename" onfocus="this.oldValue = this.value;" onchange="filenameChanged(this);this.oldValue = this.value;" size="64" value="'+elem+'" class="nothing"/></li>'
+    const val = `<li>
+      <a href="../data/${encodeURIComponent(elem)}">🔎</a>
+      <a href="../data/${encodeURIComponent(elem)}?contentType=text">📖</a> 
+      <a href="edit/${encodeURIComponent(elem)}">✏️</a> 
+      &nbsp;
+      &#x1f4c4;
+      <input type="text" name="filename" onfocus="this.oldValue = this.value;"
+             onchange="filenameChanged(this);this.oldValue = this.value;" size="64" value="${elem}" class="nothing"/>
+    </li>`
     return val
   })
   $("#root").html(items.join(''))
