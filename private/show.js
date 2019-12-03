bd1a26ec0d488ef0fc8df6d4481280252985d6b9
Pascal Rainville <pascal.rainville@gmail.com>
2019-12-03 13:09:38 -0500

    ...

--- private/show.js
+++ private/show.js
@@ -31,17 +31,30 @@ const filename = decodeURIComponent(htmlFilename)
 class ShowNav extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {showActionDropdown: false, marginWidth: '0%'}
+    this.state = {showActionDropdown: false, marginWidth: '0%', suggestions: []}
   }
 
+  suggestionList = () => {
+    var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla"]
+   
+    //return (this.state.suggestions || []).map(c => (e('div',null,c)))
+    //return (this.state.suggestions || []).map(c => (e('div',null,e('a',{href: `/show/${encodeURIComponent(c)}`},c))))
+    return (this.state.suggestions || []).map(c => (e('div',{style: {cursor: 'pointer'}, onClick: () => {window.location.href = `/show/${encodeURIComponent(c)}`}}, c)))
+  }
+
+  handleSuggestionInput = (event) => {
+    const query = event.target.value
+    $.getJSON(`http://localhost:3000/search/${query}`, ({data, shouldUseSearchEngine}) => {
+      this.setState({suggestions: data})
+    })
+  }
   render() {
     const {showActionDropdown, marginWidth} = this.state
-   
+
     return e('div', {className: 'navbar'},
       e('a', {href: 'http://localhost:3000/'}, 'Home'),
       e('a', {href: `http://localhost:3000/edit/${encodeURIComponent(filename)}`}, 'Edit'),
-      e('span', {className: 'menubtn', onClick: () => deleteFile(filename, () => {window.location.href = "/"})}, 'Delete'),
-      e('a', {href: `http://localhost:3000/publish/${encodeURIComponent(filename)}`}, 'Publish'),
+      //e('a', {href: `http://localhost:3000/publish/${encodeURIComponent(filename)}`}, 'Publish'),
       e('span', {className: 'dropdown'},
         e('button', {className: 'dropbtn', onClick: () => {this.setState({showActionDropdown: !showActionDropdown})}}, 'Actions', e('i', {className: 'fa fa-caret-down'})),
         showActionDropdown ? e('div', {id: 'myDropdown', className: 'dropdown-content'},
@@ -49,7 +62,7 @@ class ShowNav extends React.Component {
           e('div', null, 'Show 2'),
         ) : null,
       ),
-      e('span', {onClick: () => {
+      /*e('span', {onClick: () => {
         if (marginWidth === '0%') {
           $('#root').css('margin-left', '15%')
           $('#root').css('margin-right', '15%')
@@ -59,8 +72,17 @@ class ShowNav extends React.Component {
           $('#root').css('margin-right', '0%')
           this.setState({marginWidth: '0%'})
         }
-      }}, rIcon('aspect_ratio-24px.svg')),
-      e('input', {id: 'filterVal2', onKeyDown: this.onKeyDown, type: 'text', value: this.state.query, onChange: ({target}) => {this.setState({query: target.value})}}),
+      }}, rIcon('aspect_ratio-24px.svg')),*/
+      //e('input', {id: 'filterVal2', onKeyDown: this.onKeyDown, type: 'text', value: this.state.query, onChange: ({target}) => {this.setState({query: target.value})}}),
+      e('form', {autoComplete: 'off', action: '/fooooooooo', style: {display: 'inline-block', width: '800px'}},
+        e('span', {className: "autocomplete", style: {width: '100%'}},
+          e('input', {id: 'filterVal2', type: 'text', name: 'query', placeholder: 'search', onChange: this.handleSuggestionInput}),
+          e('div', {className: "autocomplete-items"},
+            this.suggestionList(),
+          ),
+        ),
+      ),
+      e('span', {className: 'menubtn', onClick: () => deleteFile(filename, () => {window.location.href = "/"})}, 'Delete'),
     )
   }
 }
