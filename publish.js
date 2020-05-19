9f8927adbbc161263e1cd5197d57993940eb50cf
Pascal Rainville <pascal.rainville@gmail.com>
2020-05-19 16:57:43 -0400

    ...

--- publish.js
+++ publish.js
@@ -3,25 +3,29 @@ const fs = require('fs');
 const path = require('path');
 var _ = require('./common/lodash.min.js')
 
-var output_dir = '../public_pascalr/'
+var output_root = '../public_pascalr/'
+var output_dir = '../public_pascalr/show'
   
-shell(`rm -R ${output_dir}/*`);
+shell(`mkdir -p ${output_dir}`);
+shell(`rm -R ${output_dir}`);
+shell(`mkdir -p ${output_dir}`);
 
 var data_list = [
-  'desktop',
   'calc',
   "Liste d'épicerie",
 ]
-//shell(`mkdir -p ${path.join(output_dir,'data')}`);
 
 var list = [...data_list]
 console.log(list)
 
 for (let i = 0; i < list.length; i++) {
-  fs.copyFile(path.join('data',list[i]), path.join(output_dir,list[i])+'.html', (err) => {
-    if (err) throw err;
-  });
+  shell(`cp "${path.join('data',list[i])}" "${path.join(output_dir,list[i])+'.html'}"`);
+  //fs.copyFile(path.join('data',list[i]), path.join(output_dir,list[i])+'.html', (err) => {
+  //  if (err) throw err;
+  //});
 }
+
+shell(`cp data/desktop ../public_pascalr/`);
 
 function cpDir(dir) {
   var output_path = path.join(output_dir,dir)
