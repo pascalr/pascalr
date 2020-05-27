f82924e00871c4996a2b5fb39d946a2140c7be3c
Pascal Rainville <pascal.rainville@gmail.com>
2020-05-27 12:55:01 +0000

    ...

--- machine.js
+++ machine.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
 const fs = require('fs');
 const path = require('path');
-const multer = require('multer')
 var _ = require('./common/lodash.min.js')
-var he = require('he') // unused I think
-var elasticlunr = require('./common/elasticlunr.js')
-const { Transform } = require('stream');
 
 var sys = require('util')
 var exec = require('child_process').exec;
@@ -19,8 +14,9 @@ const SerialPort = require('serialport');
 //})
 
 app.get('/hello',function (req, res) {
+  console.log('Received command hello')
   //res.sendFile(path.join(__dirname, req.path));
-  res.sendFile(path.join(__dirname, 'hello.html'));
+  res.sendFile(path.join(__dirname, 'data/Ma machine'));
 })
 
 // https://stackoverflow.com/questions/16333790/node-js-quick-file-server-static-files-over-http
